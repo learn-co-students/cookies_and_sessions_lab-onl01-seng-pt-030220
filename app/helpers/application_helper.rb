d92ff9c000d7c40a1e77664904c554473f13147f
Rkebede <rutht.kebede@gmail.com>
2020-08-06 16:53:09 -0500

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+ 
+  def current_cart
+    session[:cart] = [] unless session[:cart]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources :products
+  post '/products', to: 'products#add', as: "products"
+  root 'products#index'
 end
